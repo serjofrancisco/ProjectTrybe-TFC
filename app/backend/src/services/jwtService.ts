f79c2418ae50f1a037200f282cc8b04e6bbdc9ac
Sergio Francisco <spinheiro.lf@gmail.com>
2022-08-18 13:34:21 -0300

    fix: :bug: Terminando a função de jwt service

--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -1,7 +1,8 @@
 import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export default class JwtService {
   static sign(payload: { id: number, email: string }): string {
-    return jwt.sign(payload, 'SENHA');
+    return jwt.sign(payload, process.env.JWT_SECRET || 'MinhaSenha', { expiresIn: '30d' });
   }
 }
