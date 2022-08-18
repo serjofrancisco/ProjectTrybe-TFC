0da87f2fe394fc95e6c5d6af8c403f0b6529ca3a
Sergio Francisco <spinheiro.lf@gmail.com>
2022-08-18 13:36:21 -0300

    fix: :bug: Criando interface do Login

--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -1,8 +1,9 @@
 import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import ILogin from '../interfaces/ILogin';
 
 export default class JwtService {
-  static sign(payload: { id: number, email: string }): string {
+  static sign(payload: ILogin): string {
     return jwt.sign(payload, process.env.JWT_SECRET || 'MinhaSenha', { expiresIn: '30d' });
   }
 }
