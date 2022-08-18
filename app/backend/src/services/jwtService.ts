d4253727400447c4928c7953995c1ddbfe6806da
Sergio Francisco <spinheiro.lf@gmail.com>
2022-08-18 18:03:33 -0300

    feat: Verificando login no banco de dados

--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -6,4 +6,9 @@ export default class JwtService {
   static sign(payload: ILogin): string {
     return jwt.sign(payload, process.env.JWT_SECRET || 'MinhaSenha', { expiresIn: '30d' });
   }
+
+  static decode(token: string) {
+    const data = jwt.verify(token, process.env.JWT_SECRET || 'MinhaSenha');
+    return data as ILogin;
+  }
 }
--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -1,8 +1,17 @@
 import ILogin from '../interfaces/ILogin';
 import JwtService from './jwtService';
+import Users from '../database/models/user';
+import CustomError from '../middlewares/CustomError';
+import PasswordHandler from '../utils/passwordHandler';
 
 export default class LoginService {
   static async sign(payload: ILogin): Promise<string> {
+    const email = await Users.findOne({ where: { email: payload.email } });
+
+    if (!email) throw new CustomError(401, 'Incorrect email or password');
+
+    PasswordHandler.passwordCompare(payload.password, email.password);
+
     const token = JwtService.sign(payload);
     return token;
   }
